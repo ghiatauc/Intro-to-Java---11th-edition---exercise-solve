2ca80f6795fdcb099c612bd6589d2e2335d07acf
ghiatauc <119630435+ghiatauc@users.noreply.github.com>
2023-02-24 14:38:41 +0000

    Create exercise_01.java
    (Display three messages) Write a program that displays Welcome to Java, Learning Java Now, and Programming is fun.

---
+++ Chapter01/exercise_01.java
@@ -0,0 +1,13 @@
+package chapter01_ex;
+
+public class exercise_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+		System.out.println("Learning Java Now");
+		System.out.println("Programming is fun");
+
+	}
+
+}
