b20cf1f27d3044c16d7a260ee6a12500774fe05b
ghiatauc <119630435+ghiatauc@users.noreply.github.com>
2023-02-24 19:34:49 +0000

    Update package-info.java

--- Chapter03/package-info.java
+++ Chapter03/package-info.java
@@ -1,1 +1,11 @@
-
+/**
+ * <b>
+ * "Introduction to Java Programming" by Y. Daniel Liang 11th Edition
+ * <b>
+ * <p>
+ * Chapter 03 - Introduction to Computers, Programs, and Java
+ * </p>
+ *
+ * @author ghiatauc
+ */
+package chapter03_ex;
