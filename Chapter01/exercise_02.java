61e95a03ac0f19d9e12c0f972279dbe625dcd2bf
ghiatauc <119630435+ghiatauc@users.noreply.github.com>
2023-02-24 14:42:57 +0000

    Create exercise_02.java

---
+++ Chapter01/exercise_02.java
@@ -0,0 +1,16 @@
+package chapter01_ex;
+
+public class exercise_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I love Java");
+		System.out.println("I love Java");
+		System.out.println("I love Java");
+		System.out.println("I love Java");
+		System.out.println("I love Java");
+	}
+
+}
+
+//(Display five messages) Write a program that displays I love Java five times.
