b3088a6cb0d0d6d847272bc054b671761eea34c3
ghiatauc <119630435+ghiatauc@users.noreply.github.com>
2023-02-24 14:50:40 +0000

    Create exercise_05.java

---
+++ Chapter01/exercise_05.java
@@ -0,0 +1,16 @@
+package chapter01_ex;
+
+public class exercise_05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x;
+		x = (7.5 * 6.5 - 4.5 * 3)/(47.5 - 5.5);
+		
+		System.out.println("Result is "+x);
+	}
+
+}
+/*
+(Compute expressions) Write a program that displays the result of 7.5 * 6.5 - 4.5 * 3 / 47.5 - 5.5
+*/
