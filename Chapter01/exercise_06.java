35515c28acb9d7a152a5ab78eb33aa02073c0d66
ghiatauc <119630435+ghiatauc@users.noreply.github.com>
2023-02-24 15:26:03 +0000

    Update exercise_06.java

--- Chapter01/exercise_06.java
+++ Chapter01/exercise_06.java
@@ -12,4 +12,5 @@ public class exercise_06 {
 	}
 
 }
-//(Summation of a series) Write a program that displays the result of 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+//(Summation of a series)
+//Write a program that displays the result of 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
