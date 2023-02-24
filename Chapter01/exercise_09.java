75d9a64aad3a486c050b9b57d75833e45477b522
ghiatauc <119630435+ghiatauc@users.noreply.github.com>
2023-02-24 15:10:44 +0000

    Create exercise_09.java

---
+++ Chapter01/exercise_09.java
@@ -0,0 +1,23 @@
+package chapter01_ex;
+
+public class exercise_09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double width = 5.3;
+		double height = 8.6;
+		
+		double area = width * height;
+    double perimeter = 2 * (width + height);
+		
+		System.out.println("The area of rectangle with width of 5.3 and height of 8.6 is " + area);
+    System.out.println("The perimeter of rectangle with width of 5.3 and height of 8.6 is " + perimeter);
+	}
+
+}
+/*
+(Area and perimeter of a rectangle)
+Write a program that displays the area and perimeter of a rectangle with a width of 5.3 and height of 8.6 using the following formula:
+					area = width * height
+			perimeter = 2 * (width + height)
+*/
