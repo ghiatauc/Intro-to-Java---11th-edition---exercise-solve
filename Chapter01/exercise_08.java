0995f25050d8ca6bdc0e471f99b9e272a76a76a3
ghiatauc <119630435+ghiatauc@users.noreply.github.com>
2023-02-24 15:03:09 +0000

    Create exercise_08.java

---
+++ Chapter01/exercise_08.java
@@ -0,0 +1,28 @@
+package chapter01_ex;
+
+public class exercise_08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double pi = 3.14159;
+		double radius = 6.5;
+		
+		double perimeter;
+		double area;
+		
+		perimeter = 2 * radius * pi;
+		
+		area = radius * radius * pi;
+		
+		System.out.println("Perimeter is "+perimeter);
+		System.out.println("Area is "+area);
+	}
+
+}
+/*
+(Area and perimeter of a circle)
+Write a program that displays the area and perimeter of a circle that has a radius of 6.5 using the following formula:
+										pi = 3.14159
+								perimeter = 2 * radius * p
+								area = radius * radius * p
+*/
