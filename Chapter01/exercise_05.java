c73d1001f523937723fb8df785f0e35bdf469799
ghiatauc <119630435+ghiatauc@users.noreply.github.com>
2023-02-24 15:25:15 +0000

    Update exercise_05.java

--- Chapter01/exercise_05.java
+++ Chapter01/exercise_05.java
@@ -12,5 +12,6 @@ public class exercise_05 {
 
 }
 /*
-(Compute expressions) Write a program that displays the result of 7.5 * 6.5 - 4.5 * 3 / 47.5 - 5.5
+(Compute expressions)
+Write a program that displays the result of 7.5 * 6.5 - 4.5 * 3 / 47.5 - 5.5
 */
