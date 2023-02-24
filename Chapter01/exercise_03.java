5f2feac2b9047693ea5ca53e8ddf6e7314c6472e
ghiatauc <119630435+ghiatauc@users.noreply.github.com>
2023-02-24 14:45:59 +0000

    Create exercise_03.java

---
+++ Chapter01/exercise_03.java
@@ -0,0 +1,21 @@
+package chapter01_ex;
+
+public class exercise_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	  System.out.println("   J");
+	  System.out.println("J aaa   v     vaaa");
+	  System.out.println("J J  aa   v v    a a");
+	  System.out.println("J  aaaa    v     aaaa");
+	}
+
+}
+
+/*
+(Display a pattern) Write a program that displays the following pattern:
+	 J 
+	J aaa v vaaa
+	J J aa v v a a 
+	J aaaa v aaaa
+*/
