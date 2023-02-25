6aabc4597a3daf21098baa5fa420d5672cf6a098
ghiatauc <119630435+ghiatauc@users.noreply.github.com>
2023-02-25 15:12:01 +0000

    Update package-info.java

--- Chapter04/package-info.java
+++ Chapter04/package-info.java
@@ -1,3 +1,11 @@
-
-
-Mathematical Functions, Characters, and Strings
+/**
+ * <b>
+ * "Introduction to Java Programming" by Y. Daniel Liang 11th Edition
+ * <b>
+ * <p>
+ * Chapter 03 - Mathematical Functions, Characters, and Strings
+ * </p>
+ *
+ * @author ghiatauc
+ */
+package chapter04_ex;
