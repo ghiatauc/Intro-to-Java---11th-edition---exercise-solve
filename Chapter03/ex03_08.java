09d40c73ec94d355a1b11fab613d01ff8f73d900
ghiatauc <119630435+ghiatauc@users.noreply.github.com>
2023-02-25 13:54:45 +0000

    Update ex03_08.java

--- Chapter03/ex03_08.java
+++ Chapter03/ex03_08.java
@@ -32,29 +32,6 @@ public class ex03_08 {
 		}
 		
 		System.out.println(n1+" "+n2+" "+n3);
-		
-		/*
-		if(n1 <= n2) {
-			if(n2 <= n3) {
-				System.out.println(n1+" "+n2+" "+n3);
-			}else {
-				System.out.println(n1+" "+n3+" "+n2);
-			}
-		}else if(n2 <= n3) {
-			if(n1 <= n3) {
-				System.out.println(n2+" "+n1+" "+n3);
-			}else {
-				System.out.println(n2+" "+n3+" "+n1);
-			}
-		}else {
-			if(n1 <= n2) {
-				System.out.println(n3+" "+n1+" "+n2);
-			}else {
-				System.out.println(n3+" "+n2+" "+n1);
-			}
-		}
-		*/
-		
 
 	}
 
