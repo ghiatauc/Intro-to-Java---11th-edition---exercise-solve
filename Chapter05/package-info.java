ba779328a845b363920ef369361d5ba0034d0faa
ghiatauc <119630435+ghiatauc@users.noreply.github.com>
2023-04-01 15:41:44 +0100

    Create package-info.java

---
+++ Chapter05/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <b>
+ * "Introduction to Java Programming" by Y. Daniel Liang 11th Edition
+ * <b>
+ * <p>
+ * Chapter 05 - Mathematical Functions, Characters, and Strings
+ * </p>
+ *
+ * @author ghiatauc
+ */
+package chapter05_ex;
