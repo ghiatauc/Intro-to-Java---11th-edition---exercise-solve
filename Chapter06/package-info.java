0ea6bf713ae2bcc91cbb5bb88f06dc396fb41c9f
ghiatauc <119630435+ghiatauc@users.noreply.github.com>
2023-04-01 15:51:19 +0100

    Create package-info.java

---
+++ Chapter06/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <b>
+ * "Introduction to Java Programming" by Y. Daniel Liang 11th Edition
+ * <b>
+ * <p>
+ * Chapter 06 - Methods
+ * </p>
+ *
+ * @author ghiatauc
+ */
+package chapter05_ex;
